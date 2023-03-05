15806103b70fc28ce3795b9f1d508fae7e08a494
Grantas <granto.mail@gmail.com>
2023-03-05 08:45:49 +0200

    chore: added sport event type

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,13 @@
 use scraper::{Html, Selector};
 
+#[derive(Debug)]
+struct SportEvent {
+    team1: String,
+    team2: String,
+    kof1: String,
+    kof2: String,
+}
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let resp = reqwest::blocking::get("https://www.topsport.lt/krepsinis/eurolyga")?.text()?;
 
@@ -18,14 +26,28 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             .attr("content")
             .unwrap();
 
-        println!("{}", event_name);
+        let team_names = event_name.split(" - ").collect::<Vec<&str>>();
+
+        let team1 = team_names.get(0).unwrap();
+        let team2 = team_names.get(1).unwrap();        
+
+        // println!("{}", event_name);
 
         let kofs = event_element
             .select(&Selector::parse("span.prelive-list-league-rate").unwrap())
             .map(|span| span.inner_html())
             .collect::<Vec<_>>();
 
-        println!("{:#?}", kofs);
+        // println!("{:#?}", kofs);
+
+        let sport_event = SportEvent {
+            team1: team1.to_string(),
+            team2: team2.to_string(),
+            kof1: kofs[0].clone(),
+            kof2: kofs[1].clone(),
+        };        
+
+        println!("{:#?}", sport_event);
     }
 
     let elements = document.select(&selector).collect::<Vec<_>>();
